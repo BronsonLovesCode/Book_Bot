0d35aa4d081b56ea1eb05079682c0dc6090349bb
BronsonLovesCode <132307562+BronsonLovesCode@users.noreply.github.com>
2023-06-19 12:24:37 +1200

    Integrated welcome message into main function
    Integrated welcome message into main function.

---
+++ components/welcome v1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of random names
+names = ["Bronson", "Adam", "Alain", "Ethan", "Eardwulf", "Santiago", "Daniel", "Dannell", "Luka", "Jason"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("Welcome to COMTIC!")
+print("My name is",name,)
+print("I will be here to help you order your ideal book.")
