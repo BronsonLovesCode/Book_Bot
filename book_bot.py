2cd210445bbf655149201481269e0039105224c4
BronsonLovesCode <132307562+BronsonLovesCode@users.noreply.github.com>
2023-06-23 10:15:01 +1200

    integrated pickup/delivery menu
    Integrated pickup/delivery menu into main program.

--- book_bot.py
+++ book_bot.py
@@ -20,6 +20,32 @@ def welcome():
     print()
     print("I will be here to help you order your ideal book.")
 
+# Menu so that users can choose either pickup or delivery
+
+def order_type():
+    print ("Is your order for pickup or delivery?")
+    print()
+    print ("For pickup, enter 1.")
+    print ("For delivery, enter 2.")
+
+    while True:
+        try:
+                delivery = int(input("Please enter a number. "))
+                if delivery >= 1 and delivery <= 2:
+                    if delivery == 1:
+                            print ("Pickup")
+                            break
+
+                    elif delivery == 2:
+                            print ("Delivery")
+                            break
+                else:
+                    print ("Number must be 1 or 2.")
+        except ValueError:
+                print ("*** INVALID INPUT ***")
+                print ("Please enter 1 or 2. ")
+
+# Main function
 def main():
     '''
     Purpose: To run all functions
@@ -29,5 +55,6 @@ def main():
 
     '''
     welcome()
+    order_type()
 
 main()
